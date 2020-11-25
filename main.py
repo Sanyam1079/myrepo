a70aebebb4912969601db5c02aba3743d6384385
Sanyam1079 <59022233+Sanyam1079@users.noreply.github.com>
2020-11-25 08:11:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Model import spatioTemporalClassifier
 from Dataloaders import getDataloader, ufctraintest
 
 #if frame counts are different then batch size can only be 1
-data = getDataloader('path', batch=2, workers=6, frames=102)
+data = getDataloader('../input/ucf101', batch=2, workers=6, frames=102)
 model = spatioTemporalClassifier(classes=2)
 model.train_model(model=model, dataloader=data, epochs=2)

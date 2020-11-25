85393574718e3cb4dadcb86332e43ecf5bfa7634
Sanyam1079 <59022233+Sanyam1079@users.noreply.github.com>
2020-11-25 16:58:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#importing the codes from other .py files
 from Model import spatioTemporalClassifier
 from Dataloaders import getDataloader, ufctraintest
-
+#main fucntion calls to train the model 
 #if frame counts are different then batch size can only be 1
 data = getDataloader('/kaggle/input/ucf101/UCF101/UCF-101', batch=2, workers=6, frames=102)
 model = spatioTemporalClassifier(classes=2)
